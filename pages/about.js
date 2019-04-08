dd9cd4ca56ed2525fde0cde849bcb66997790aca
Benjamin Löffel <benjaminloeffel@gmail.com>
2019-04-08 15:15:56 +0200

    - Created sample pages

---
+++ pages/about.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => <Layout title="About">About</Layout>;
+
+export default About;
---
+++ pages/account.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Account = () => <Layout title="Account">Account</Layout>;
+
+export default Account;
