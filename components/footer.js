6c7eef98f245575fb570d52cc001154f770ca772
Benjamin Löffel <benjaminloeffel@gmail.com>
2019-04-08 15:13:47 +0200

    - Created basic "Footer" component with style

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <small>&copy; Example {new Date().getFullYear()}</small>
+      <style jsx>{`
+        background: gray;
+        padding: 20px 10px;
+        text-align: center;
+
+        small {
+          padding: 0;
+        }
+      `}</style>
+    </footer>
+  );
 };
 
 export default Footer;
