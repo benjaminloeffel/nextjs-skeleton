13602678787cbf5e037587137031fbd5deca443f
Benjamin Löffel <benjaminloeffel@gmail.com>
2019-04-08 15:14:14 +0200

    - Created basic "Header" component with style

--- components/header.js
+++ components/header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import AppBar from '../components/appbar';
+import Hero from '../components/hero';
 
-const Header = () => {
-  return <header>Header</header>;
+const Header = props => {
+  return (
+    <header>
+      <AppBar />
+      <Hero title={props.title} />
+    </header>
+  );
 };
 
 export default Header;
